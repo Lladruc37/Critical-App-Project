6c569ac07b9a47fa9567b3e4ba731b7e6dfff24c
Lladruc <59049906+Lladruc37@users.noreply.github.com>
2021-12-16 12:25:41 +0100

    Implemented login/register/logout
    Co-Authored-By: WittIsHere <59049883+WittIsHere@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,60 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'app.dart';
+import 'widgets/auth_gate.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final db = FirebaseFirestore.instance;
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: StreamBuilder(
+//           stream: db.doc("/chats/jhGZ6Bx7EvKocNdvLUVU").snapshots(),
+//           builder: (
+//             BuildContext context,
+//             AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot,
+//           ) {
+//             if (!snapshot.hasData) {
+//               return const Center(
+//                 child: CircularProgressIndicator(),
+//               );
+//             }
+//             final doc = snapshot.data!.data();
+//             if (doc != null) {
+//               return Center(
+//                 child: Text(doc['title']),
+//               );
+//             } else {
+//               return const Center(
+//                 child: Text("doc is null"),
+//               );
+//             }
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final db = FirebaseFirestore.instance;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: StreamBuilder(
-          stream: db.doc("/testy/DqN3N9Fpz7O8gMxYrXem").snapshots(),
-          builder: (
-            BuildContext context,
-            AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot,
-          ) {
-            if (!snapshot.hasData) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final doc = snapshot.data!.data();
-            if (doc != null) {
-              return Center(
-                child: Text(doc['text']),
-              );
-            } else {
-              return const Center(
-                child: Text("doc is null"),
-              );
-            }
-          },
-        ),
-      ),
-    );
-  }
+  // await dotenv.load(fileName: '.env');
+  runApp(
+    const AuthGate(
+      app: App(),
+    ),
+  );
 }
---
+++ lib/widgets/auth_gate.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+
+class AuthGate extends StatelessWidget {
+  final Widget app;
+  const AuthGate({Key? key, required this.app}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+        if (!snapshot.hasData) {
+          return const MaterialApp(
+            home: SignInScreen(
+              providerConfigs: [
+                EmailProviderConfiguration(),
+                // GoogleProviderConfiguration(
+                //   clientId: dotenv.env['GOOGLE_SIGNIN_CLIENT_ID'] ?? "",
+                // ),
+              ],
+            ),
+          );
+        }
+        return app;
+      },
+    );
+  }
+}
